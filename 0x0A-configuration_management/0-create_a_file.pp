c3ae9c756ff5bdacb36f2049a6f663001e6027ba
aarizat <aarizatr@gmail.com>
2020-12-07 20:57:56 +0000

    Delete indentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Create file
+
 file {'/tmp/holberton':
-    content => 'I love Puppet',
-    owner => 'www-data',
-    group => 'www-data',
-    mode => '0744',
+content => 'I love Puppet',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 }
