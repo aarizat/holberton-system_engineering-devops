b2b6485751d7e56ba98a84ac40717fe2a690fa12
aarizat <aarizatr@gmail.com>
2020-12-07 20:54:15 +0000

    Put path at the beginning

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # Create file
-
-file {'holberton':
+file {'/tmp/holberton':
     ensure => 'present',
     owner => 'www-data',
     group => 'www-data',
     mode => '0744',
     content => 'I love Puppet',
-    path => '/tmp/holberton',
 }
