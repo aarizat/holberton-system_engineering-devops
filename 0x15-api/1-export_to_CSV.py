bd97e8c761ac05cba26a245df0e8a1b12d5dcefc
aarizat <aarizatr@gmail.com>
2021-02-01 19:49:02 +0000

    Export user's data for csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Return info about an user for a given ID to csv file -- REST API
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = requests.get("https://jsonplaceholder.typicode.com/users?id={}"
+                        .format(sys.argv[1])).json()
+    posts = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(sys.argv[1])).json()
+    username = user[0].get("username")
+    data = [(p.get("userId"), username, p.get("completed"), p.get("title"))
+            for p in posts]
+    with open("{}.csv".format(sys.argv[1]), mode="w") as out:
+        write = csv.writer(out)
+        write.writerows(data)
