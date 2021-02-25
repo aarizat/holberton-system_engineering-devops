13fc608229a4d7f325126da31335b0f2ef9c7c42
aarizat <aarizatr@gmail.com>
2021-02-25 19:46:45 +0000

    Replace simple quote by double quote

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # Fix request limits
 
 exec { 'fix-limits':
-  command => 'sed -i s/15/4096/g /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-}
-
-exec { 'restart':
-  command => 'service nginx restart',
+  command => "sed -i 's/15/4096/g' /etc/default/nginx; service nginx restart",
   path    => '/usr/local/bin/:/bin/'
 }
